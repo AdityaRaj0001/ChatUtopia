ca64bd6c6457000249639d0f92fcca26139d502d
AdityaRaj0001 <meadityaraj0001@gmail.com>
2023-10-26 02:07:52 +0530

    add final files for basic app

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from '@/components/Home/Home'
+import React from 'react'
+import { useStore } from '../../suppliers/zustand'
+
+const index = () => {
+  return (
+    <Home/>
+  )
+}
+
+export default index
